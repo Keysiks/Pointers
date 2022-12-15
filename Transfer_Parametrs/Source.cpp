072dbf4dd7f1c94227884ece9c4101acad6ef271
Кирилл <kirillredko59@gmail.com>
2022-12-15 20:47:50 +0300

    by pointer

--- Transfer_Parametrs/Source.cpp
+++ Transfer_Parametrs/Source.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 
-void exchange(int a, int b) {
-	int temp = a;
-	a = b;
-	b = temp;
+void exchange(int* a, int* b) { 
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 void main() {
 	int a = 2; int b = 3;
 	cout << a << " " << b << endl;
-	exchange(a, b);
+	exchange(&a, &b);
 	cout << a << " " << b << endl;
 }
